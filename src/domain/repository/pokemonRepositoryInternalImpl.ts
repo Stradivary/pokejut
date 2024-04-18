ca7f2d20e19b343792b02681dbe84dbfed592a0c
stradivary <ramouz.muzacky@gmail.com>
2024-04-18 12:12:44 +0700

    Move Pokemon Data

--- src/domain/repository/pokemonRepositoryInternalImpl.ts
+++ src/domain/repository/pokemonRepositoryInternalImpl.ts
@@ -71,6 +71,6 @@ class PokemonRepository {
 }
 
 
-const jsonPokemons = await import("../../data/pokemon_list.json");
+const jsonPokemons = await import("../../data/raw-data/pokemon_list.json");
 
 export const pokemonInternalRepo = new PokemonRepository(jsonPokemons.pokemons);
